307c7e1d450000a4a6bb59045ba6391085b60c7f
Achmad Fauzi <achmadfauzi@live.com>
2022-04-28 17:00:46 +0700

    add comment and clear code unused

--- index.py
+++ index.py
@@ -1,9 +1,12 @@
 import json
 
+# set json sesuai soal
 data = '{ "name":"Fauzi", "promo":"First Buy Promo", "link":"https://google.com" }'
-data_object = json.loads(data)
-pairs = data_object.items()
 
+# load menjadi object
+data_object = json.loads(data)
+
+# tapi pada case ini tanpa looping pun berhasil
 message = 'Halo '+data_object['name']+', ini ada promo '+data_object['promo']+', bisa di klik di link '+ data_object['link']
 
 print(message)
